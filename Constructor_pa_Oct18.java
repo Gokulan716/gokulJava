1426294122749d5419d64c14ce253c986d1a3c05
Gokulan M <gokulan.m@symphonyai.com>
2024-10-18 19:46:29 +0530

    push form windows OCT_18

---
+++ Constructor_pa_Oct18.java
@@ -0,0 +1,11 @@
+public class Constructor_pa_Oct18 {
+    Constructor_pa_Oct18(){
+
+        System.out.println("Constructor call");
+    }
+    public static void main(String[] args) {
+
+        Constructor_pa_Oct18 con = new Constructor_pa_Oct18();
+        Constructor_pa_Oct18 con2 = new Constructor_pa_Oct18();   
+    }
+}
